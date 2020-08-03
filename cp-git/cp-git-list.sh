a98db1a11657d1d9261cc0dabd826d5bd0791220
J. Gabriel Azevedo <46544597+Wultyc@users.noreply.github.com>
2020-08-03 19:45:36 +0000

    Update cp-git-list.sh

--- cp-git/cp-git-list.sh
+++ cp-git/cp-git-list.sh
@@ -1,5 +1,17 @@
 #!/bin/sh
 
+# cp-git: This script run cp-git on multiple repos at once
+# usage:
+#   ./cp-git-list.sh <list filename>
+#
+# List file format
+# each row is one pair of repository and new remote url seperated by a space
+# Example:
+#
+# REPO_1 http://example.com/repo_1.git
+# REPO_2 http://example.com/repo_3.git
+# REPO_3 http://example.com/repo_2.git
+
 input=$1
 
 while IFS=' ' read -r repo url
